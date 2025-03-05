04f4d0bd5ff20e537f6df8e98cde3326ae0c0a39
Liam Harrison <liam.harrison@mymail.champlain.edu>
2025-03-05 09:38:29 -0500

    basic movement

--- Solo-Project/Assets/Scripts/PlayerMovement.cs
+++ Solo-Project/Assets/Scripts/PlayerMovement.cs
@@ -6,19 +6,45 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [SerializeField] private InputSystem_Actions inputActions;
-    
+    [SerializeField] private float movementSpeed = 10f;
+    [SerializeField] private Transform orientation;
+
+    private Vector2 movementInput;
+    private Vector3 movementDirection;
+    private Vector3 appliedForce;
+
+    private void Awake()
+    {
+        inputActions = new InputSystem_Actions();
+    }
+
     private void OnEnable()
     {
-        InputManager.OnPlayerMovement += UpdateMovement;
+        inputActions.Player.Enable();
     }
 
     private void OnDisable()
     {
-        InputManager.OnPlayerMovement -= UpdateMovement;
+        inputActions.Player.Disable();
     }
 
-    private void UpdateMovement(InputAction.CallbackContext context)
+    private void FixedUpdate()
     {
+        movementInput = inputActions.Player.Move.ReadValue<Vector2>();
+        Vector3 velocity = new Vector3(movementInput.x * movementSpeed, rb.linearVelocity.z, movementInput.y * movementSpeed);
+        rb.linearVelocity = transform.TransformDirection(velocity);
         
+        
+        // movementInput.y = 0f;
+        // movementDirection = orientation.forward + orientation.right * movementInput.x;
+        //
+        // appliedForce = movementDirection.normalized * (movementSpeed * 10);
+        // rb.AddForce(appliedForce, ForceMode.Force);
+        //rb.linearVelocity = movementInput * movementSpeed;
+    }
+
+    private void OnMove(InputValue value)
+    {
+        //movementInput = value.Get<Vector2>();
     }
 }
